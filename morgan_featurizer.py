be0dd065f3d2d4f742cf7502a83c60658e74d19e
JHlozek <jason.hlozek@uct.ac.za>
2024-10-16 17:09:50 +0200

    Remove invalid smiles

--- morgan_featurizer.py
+++ morgan_featurizer.py
@@ -5,19 +5,7 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-class Featurizer(ABC):
-    def featurize(self: "Featurizer", batch: Any) -> Any:
-        """Featurize input batch.
-
-        Args:
-            batch (Any): batch of smiles
-
-        Returns:
-            Any: featurized outputs
-        """
-        pass
-
-class MorganFeaturizer(Featurizer):
+class MorganFeaturizer(object):
     def __init__(self: "MorganFeaturizer") -> None:
         self.name = "morganfeaturizer"
         
@@ -31,9 +19,32 @@ class MorganFeaturizer(Featurizer):
             Any: featurized outputs
         """
         mols = [Chem.MolFromSmiles(smi) for smi in batch if smi is not None]
-        fps = [np.array(AllChem.GetMorganFingerprintAsBitVect(
-                mol, radius=3, nBits=1024))
-                if mol is not None else None
-                for mol in mols
+        ecfps = self.ecfp_counts(mols)
+        return ecfps
+    
+    def ecfp_counts(self: "MorganFeaturizer", mols: List) -> List:
+        """Create ECFPs from batch of smiles.
+
+        Args:
+            mols (List): batch of molecules
+
+        Returns:
+            List: batch of ECFPs
+        """
+        fps = [
+            AllChem.GetMorganFingerprint(
+                mol, radius=3, useCounts=True, useFeatures=True
+            ) if mol is not None else None
+            for mol in mols
         ]
-        return fps
+        
+        nfp = []
+        for fp in fps:
+            if fp is not None:
+                tmp = np.zeros((1024), np.float32)
+                for idx, v in fp.GetNonzeroElements().items():
+                    tmp[idx % 1024] += int(v)
+                nfp.append(tmp)
+            else:
+                nfp.append(None)
+        return np.array(nfp)
